78834e790338c7238d745b29111af7e3f7699e17
AJ-Abenoja <97102877+AJ-Abenoja@users.noreply.github.com>
2022-11-05 18:57:46 +0800

    Update Farm.java
    Signed-off-by: AJ-Abenoja <97102877+AJ-Abenoja@users.noreply.github.com>

--- Farm/Farm.java
+++ Farm/Farm.java
@@ -1,33 +1,42 @@
 public class Farm {
-    String farmName;
     int[][] farmField;
+    double farmLevel;
     int i, j;
 
 
     /*
         This function identifies if the one of the tiles in the field is plowed
      */
-    //public int[][] isPlowedField( int [][] arrFarmField ) {
-
-    //}
+    public void farmPlowFieldTile( int nRow, int nCol ) {
+        if(farmField[nRow-1][nCol-1] == 0) {
+            farmLevel = farmLevel + 0.5d;
+            farmField[nRow-1][nCol-1] = 1;
+            System.out.println("Add Level: 0.5");
+            System.out.println("You plowed the tile: [" + nRow + "][" + nCol + "]");
+        }
+        else {
+            System.out.println("This tile is has a plant: [" + nRow + "][" + nCol + "]");
+        }
+    }
 
     
 
     /*Getters and Setters */
-    public void setFarmName( String farmName ) {
-        this.farmName = farmName;
-    }
 
-    public String getFarmName( String farmName ) {
-        return farmName;
-    }
-
-    public void setFarmField( int[][] farField ) {
-        this.farmField = farField;
+    public void setFarmField( int[][] farmField ) {
+        this.farmField = farmField;
     }
 
     public int[][] getFarmField( ) {
         return farmField;
+    }
+
+    public void setFarmLevel( double farmLevel ) {
+        this.farmLevel = farmLevel;
+    }
+
+    public double getFarmLevel( ) {
+        return farmLevel;
     }
 
 
