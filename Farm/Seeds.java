9d1dcbdbea2e0f57c4c1279f953550d312b44822
Lexi Padua <93819445+egotisticalBlobfish@users.noreply.github.com>
2022-11-06 06:26:56 +0800

    Update Seeds.java
    Signed-off-by: Lexi Padua <93819445+egotisticalBlobfish@users.noreply.github.com>

--- Farm/Seeds.java
+++ Farm/Seeds.java
@@ -291,40 +291,4 @@ public class Seeds {
 		
 		}
 	
-
-	
-	
-	//main
-	public  void plantSeeds(String[] args) {
-		
-		Scanner sc= new Scanner(System.in);
-
-		int nTotalGoldCoins=100;
-		String strToolonHand=null;
-		String[] strSeed= {"Turnip","Carrot","Potato","Rose","Tulips","Sunflower","Mango","Apple"};
-		int[] prices= {5,10,20,5,10,20,200};
-    
-				System.out.println("Choose a seed");
-				System.out.println("0.) Turnip (5 g)");
-				System.out.println("1.) Carrot (10 g)");
-				System.out.println("2.) Potato (20 g)");
-				System.out.println("3.) Rose (5 g)");
-				System.out.println("4.) Tulips (10 g)");
-				System.out.println("5.) Sunflower (10 g)");
-				System.out.println("6.) Mango (100 g)");
-				System.out.println("7.) Apple (200)");
-				
-				int nSeedChoice=sc.nextInt();
-				
-				System.out.println("You chose \n"+strSeed[nSeedChoice]+"!");
-				
-				String nSeedindex=strSeed[nSeedChoice];
-				
-					//initiate seed transaction
-				seeds(nSeedindex,nSeedChoice, nTotalGoldCoins,prices);
-				
-    sc.close();
-					
-				
-  }
 			
