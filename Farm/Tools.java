f8d14772ab97335d58ae31b3211913a58cf80238
AJ-Abenoja <97102877+AJ-Abenoja@users.noreply.github.com>
2022-11-06 18:14:24 +0800

    Update Tools.java
    Signed-off-by: AJ-Abenoja <97102877+AJ-Abenoja@users.noreply.github.com>

--- Farm/Tools.java
+++ Farm/Tools.java
@@ -1,65 +1,81 @@
-/*The Tools class shows the equipping of tools and the function depends on the state of the specific land tile*/
+public class Tools {
+    String[] toolList;
+    int[] toolCostUsageList;
+    double[] toolExpGainList;
+    int toolChoice;
+    int toolFarmerObjectCoins;
+    double toolFarmerLevel;
 
-public class Tools{
-	public tools(String toolInx,int totalcoins,String equippedtool,String[] Tools, int toolchoice) {
+    
+    public void toolComputeFarmerLevel( ) {
+        switch( toolChoice - 1 ) {
+            case 0:     toolFarmerLevel = toolFarmerLevel + toolExpGainList[toolChoice-1];
+                        break;
+            case 1:     toolFarmerLevel = toolFarmerLevel + toolExpGainList[toolChoice-1];
+                        break;
+            case 2:     toolFarmerLevel = toolFarmerLevel + toolExpGainList[toolChoice-1];
+                        break;
+            case 3:     toolFarmerLevel = toolFarmerLevel + toolExpGainList[toolChoice-1];
+                        break;
+            case 4:     toolFarmerLevel = toolFarmerLevel + toolExpGainList[toolChoice-1];
+                        break;
+            case 5:     toolFarmerLevel = toolFarmerLevel + toolExpGainList[toolChoice-1];
+                        break;
+            case 6:     toolFarmerLevel = toolFarmerLevel + toolExpGainList[toolChoice-1];
+                        break;
+        }
+    }
 
-		switch (toolInx) {
-		
-		case "Plow":
-			System.out.println("You chose the Plow!");
-			equippedtool="Plow";
-			System.out.println("You equipped:");
-			System.out.println(Tools[toolchoice]);
-			//insert method
-				break;
-		case "WateringCan":
-			System.out.println("You chose the Watering Can!");
-			equippedtool="Watering Can";
-			System.out.println("You equipped:");
-			System.out.println(Tools[toolchoice]);
-			//insert method
-				break;
-		case "Fertilizer":
-			if(totalcoins>=10) {
-				totalcoins=totalcoins-10;
-			System.out.println("You chose the Fertilizer!");
-			equippedtool="Fertilizer";
-			System.out.println("You equipped:");
-			System.out.println(Tools[toolchoice]);
-			//insert method
-			}
-			else {
-				System.out.println("Sorry, please try again");
-			}
-				break;
-	case "Pickaxe":
-		if(totalcoins>=50) {
-			totalcoins=totalcoins-50;
-			System.out.println("You chose the Pickaxe!");
-			equippedtool="Pickaxe";
-			System.out.println("You equipped:");
-			System.out.println(Tools[toolchoice]);
-			//insert method
-			}
-			else {
-			System.out.println("Sorry, please try again");
-			}
-				break;
-	case "Shovel":
-		if (totalcoins>=7) {
-			totalcoins=totalcoins-7;
-			System.out.println("You chose the Shovel!");
-			equippedtool="Shovel";
-			System.out.println("You equipped:");
-			System.out.println(Tools[toolchoice]);
-			//insert method
-				break;
-		}
-	default:
-		System.out.println("Invalid option");
-		break;
-		}
-		
-	}
-  
- 
+    public void toolComputeFarmerObjectCoins( ) {
+        
+    }
+    
+
+    public void setToolList( String[] toolList ) {
+        this.toolList = toolList;
+    }
+
+    public String[] getToolList( ) {
+        return toolList;
+    }
+
+    public void setToolCostUsageList( int[] toolCostUsageList ) {
+        this.toolCostUsageList = toolCostUsageList;
+    }
+
+    public int[] getToolCostUsageList( ) {
+        return toolCostUsageList;
+    }
+
+    public void setToolExpGainList( double[] toolExpGainList ) {
+        this.toolExpGainList = toolExpGainList;
+    }
+
+    public double[] getToolExpGainList( ) {
+        return toolExpGainList;
+    }
+
+    public void setToolChoice( int toolChoice ) {
+        this.toolChoice = toolChoice;
+    }
+    
+    public int getToolChoice( ) {
+        return toolChoice;
+    }
+
+    public void setToolFarmerObjectCoins( int toolFarmerObjectCoins ) {
+        this.toolFarmerObjectCoins = toolFarmerObjectCoins;
+    }
+
+    public int getToolObjectCoins( ) {
+        return toolFarmerObjectCoins;
+    }
+
+    public void setToolFarmerLevel( double toolFarmerLevel ) {
+        this.toolFarmerLevel = toolFarmerLevel;
+    }
+
+    public double getToolFarmerLevel( ) {
+        return toolFarmerLevel;
+    }
+}
